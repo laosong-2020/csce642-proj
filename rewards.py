372726549ebf36ace76825d87cc871206965e5a4
laosong-2020 <songzl0606@gmail.com>
2023-11-29 17:30:53 -0600

    change to RESCO cologne3 map

---
+++ rewards.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def wait_norm(signals):
+    def wait_norm(signals):
+    rewards = dict()
+    for signal_id in signals:
+        total_wait = 0
+        for lane in signals[signal_id].lanes:
+            total_wait += signals[signal_id].full_observation[lane]['total_wait']
+
+        rewards[signal_id] = np.clip(-total_wait/224, -4, 4).astype(np.float32)
+    return rewards
