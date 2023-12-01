b09e6917b3e2beaa25efad687d27bbdd6fa30b5b
laosong-2020 <songzl0606@gmail.com>
2023-12-01 15:13:35 -0600

    solve conflicts

--- env/map_config.py
+++ env/map_config.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 map_configs = {
     'cologne1': {
         'lights': [],
@@ -22,18 +21,4 @@ map_configs = {
         'end_time': 28800,
         'warmup': 0
     }
-=======
-map_configs = {
-    'cologne3': {
-        'lights': [],
-        'net': 'environments/cologne3/cologne3.sumocfg',
-        'route': None,
-        'step_length': 50,
-        'yellow_length': 3,
-        'step_ratio': 1,
-        'start_time': 25200,
-        'end_time': 28800,
-        'warmup': 0
-    }
->>>>>>> 372726549ebf36ace76825d87cc871206965e5a4
 }
